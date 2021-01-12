03d6e323482696a61ecf00f3f55a44aab03fd87f
caams2wangye <33855486+caams2wangye@users.noreply.github.com>
2021-01-12 15:17:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,37 @@
 import argparse
+import torch
 from utils import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', default='0,3,6,9')
+    parser.add_argument('--load',
+                        default='./experiments/exp_1/best_model.pth',
+                        help='the model path')
+    parser.add_argument('--test_iter', type=int, default=10000,
+                        help='num of tasks used to evaluate model')
+    parser.add_argument('--n-way', type=int, default=5,
+                        help='num of classes in task setting')
+    parser.add_argument('--test-shot', type=int, default=1,
+                        help='num of examples will be sampled in each cls')
+    parser.add_argument('--test-query', type=int, default=10,
+                        help='num of samples used to test in each cls')
     parser.add_argument('--network', type=str, default='Conv4',
                         help='feature extracting backbone')
     parser.add_argument('--head', type=str, default='ProtoNet',
                         help='classification head')
     parser.add_argument('--dataset', type=str, default='miniImageNet',
                         help='datasets')
-    parser.add_argument('--load',
-                        default='./experiments/exp_1/best_model.pth',
-                        help='the model path')
+    parser.add_argument('--save-path', default='./experiments/exp_1',
+                        help='save_path')
     parser.add_argument('--aug', type=bool, default=False,
                         help='whether to use augmentation method')
     parser.add_argument('--workers', type=int, default=2,
                         help='num of thread to process image')
-    parser.add_argument('--test_iter', type=int, default=5,
-                        help='num of tasks used to evaluate model')
-    parser.add_argument('--n-way', type=int, default=5,
-                        help='num of classes in task setting')
-    parser.add_argument('--test-shot', type=int, default=2,
-                        help='num of examples will be sampled in each cls')
-    parser.add_argument('--test-query', type=int, default=3,
-                        help='num of samples used to test in each cls')
-    parser.add_argument('--save-path', default='./experiments/exp_1',
-                        help='save_path')
-    parser.add_argument('--gpu', default='0')
 
     opt = parser.parse_args()
+    
+    set_gpu(opt.gpu)
 
     log_file_path = os.path.join(opt.save_path, 'test_log.txt')
     log(log_file_path, str(vars(opt)))
@@ -62,13 +65,12 @@ if __name__ == '__main__':
 
         acc = count_accuracy(logit_query.reshape(-1, n_way), labels_query.reshape(-1))
         test_accuracies.append(acc.item())
+        
 
         avg = np.mean(np.array(test_accuracies))
         std = np.std(np.array(test_accuracies))
         ci95 = 1.96 * std / np.sqrt(i + 1)
 
         if i % 10 == 0:
-            print('Episode [{}/{}]:\t\t\tAccuracy: {:.2f} ± {:.2f} % ({:.2f} %)'\
+            print('Episode [{}/{}]:\t\t\tAccuracy: {:.2f} 卤 {:.2f} % ({:.2f} %)'\
                   .format(i, opt.test_iter, avg, ci95, acc))
-
-
