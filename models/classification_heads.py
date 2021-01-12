74ccc9981444fc4dcaee1cf1505f41c4b85d9eb7
caams2wangye <33855486+caams2wangye@users.noreply.github.com>
2021-01-12 14:38:48 +0800

    Add files via upload

--- models/classification_heads.py
+++ models/classification_heads.py
@@ -24,6 +24,18 @@ def one_hot(indices, depth):
 
     return encode_indicies
 
+
+def PowerTransform(m_input, beta, epsilon):
+    support_min = torch.min(m_input)
+    support_max = torch.max(m_input)
+    support_norm = torch.div(m_input - support_min, support_max - support_min)
+    support_1 = torch.pow(support_norm + epsilon, beta)
+    support_l2 = torch.norm(support_1, dim=2)
+    a = torch.randn(1)
+    pt = torch.div(support_1, support_l2.view(support_l2.size()+a.size()))
+    return pt
+
+
 # ProtoNetHead is a just a example which used Euclidean distance to measure similarity
 # you can reference ProtoNetHead to design your own classification head
 def ProtoNetHead(query, support, support_labels, n_way, n_shot, normarlize=True):
@@ -76,32 +88,105 @@ def ProtoNetHead(query, support, support_labels, n_way, n_shot, normarlize=True)
     return logits  # (task_per_batch, n_query, n_way) return similarity score
 
 
-"""
-below are three prototype function you need to code:
-1.classifition_head1,classifition_head2 and classifition_head3 are single classifier 
-2.FusionClsHead is used to fusion results of top three classification head
-3.The meaning of input paras in each function you can reference 'ProtoNetHead' above
-if you want add trainable parameters, maybe you can use torch.nn.Parameter(torch.Tensor())
-"""
-def classifition_head1(query, support, support_labels, n_way, n_shot, normarlize=True):
-    logit1 = None
+def CosineSimilarity(query, support, support_labels, n_way, n_shot, normarlize=False):
+    """
+        Parameters:
+          query:  a (tasks_per_batch, n_query, d) Tensor.
+          support:  a (tasks_per_batch, n_support, d) Tensor.
+          support_labels: a (tasks_per_batch, n_support) Tensor.
+          n_way: a scalar. Represents the number of classes in a few-shot classification task.
+          n_shot: a scalar. Represents the number of support examples given per class.
+          normalize: a boolean. Represents whether if we want to normalize the distances by the embedding dimension.
+        Returns: a (tasks_per_batch, n_query, n_way) Tensor.
+    """
+    tasks_per_batch = query.size(0)
+    n_support = support.size(1)
+    n_query = query.size(1)
+    d = query.size(2)
+
+    assert (query.dim() == 3)
+    assert (support.dim() == 3)
+    assert (query.size(0) == support.size(0) and query.size(2) == support.size(2))
+    assert (n_support == n_way * n_shot)
+
+    # power transform
+    epsilon = 1e-7
+    beta = 0.5
+    support = PowerTransform(support, beta, epsilon)
+    query = PowerTransform(query, beta, epsilon)
+
+    support_labels_one_hot = one_hot(support_labels.view(tasks_per_batch * n_support), n_way)
+    support_labels_one_hot = support_labels_one_hot.view(tasks_per_batch, n_support, n_way)
+
+    # transpose label to make same class labels in identical counterpart
+    lables_train_transposed = support_labels_one_hot.transpose(1, 2)  # (tasks_per_batch, n_way, n_support)
+    # sum elements with same label
+    prototypes = torch.bmm(lables_train_transposed, support)  # (tasks_per_batch, n_way, d)
+    # get average
+    prototypes = prototypes.div(lables_train_transposed.sum(dim=2, keepdims=True).expand_as(prototypes))
+    AB = computeGramMatrix(query, prototypes)  # (tasks_per_batch, n_query, n_way)
+    epsilon2 = 1e-8
+    A_norm2 = torch.norm(prototypes, dim=2)
+    A_norm2 = A_norm2.view(prototypes.size(0), prototypes.size(1), 1)
+    B_norm2 = torch.norm(query, dim=2)
+    B_norm2 = B_norm2.view(query.size(0), query.size(1), 1)
+    logit1 = torch.div(AB, torch.bmm(B_norm2, A_norm2.transpose(1, 2)) + epsilon2)
+    # logit1 = -logit1
+
+    if normarlize:
+        logit1 = logit1 / d
+
     return logit1
 
 
-def classifition_head2(query, support, support_labels, n_way, n_shot, normarlize=True):
+def MahalanobisDis(query, support, support_labels, n_way, n_shot, normarlize=True):
+    """
+        Parameters:
+          query:  a (tasks_per_batch, n_query, d) Tensor.
+          support:  a (tasks_per_batch, n_support, d) Tensor.
+          support_labels: a (tasks_per_batch, n_support) Tensor.
+          n_way: a scalar. Represents the number of classes in a few-shot classification task.
+          n_shot: a scalar. Represents the number of support examples given per class.
+          normalize: a boolean. Represents whether if we want to normalize the distances by the embedding dimension.
+        Returns: a (tasks_per_batch, n_query, n_way) Tensor.
+    """
+    tasks_per_batch = query.size(0)
+    n_support = support.size(1)
+    n_query = query.size(1)
+    d = query.size(2)
+
+    assert (query.dim() == 3)
+    assert (support.dim() == 3)
+    assert (query.size(0) == support.size(0) and query.size(2) == support.size(2))
+    assert (n_support == n_way * n_shot)
+
     logit2 = None
     return logit2
 
 
-def classifition_head3(query, support, support_labels, n_way, n_shot, normarlize=True):
+def WassersteinDis(query, support, support_labels, n_way, n_shot, normarlize=True):
+    """
+        Parameters:
+          query:  a (tasks_per_batch, n_query, d) Tensor.
+          support:  a (tasks_per_batch, n_support, d) Tensor.
+          support_labels: a (tasks_per_batch, n_support) Tensor.
+          n_way: a scalar. Represents the number of classes in a few-shot classification task.
+          n_shot: a scalar. Represents the number of support examples given per class.
+          normalize: a boolean. Represents whether if we want to normalize the distances by the embedding dimension.
+        Returns: a (tasks_per_batch, n_query, n_way) Tensor.
+    """
+    tasks_per_batch = query.size(0)
+    n_support = support.size(1)
+    n_query = query.size(1)
+    d = query.size(2)
+
+    assert (query.dim() == 3)
+    assert (support.dim() == 3)
+    assert (query.size(0) == support.size(0) and query.size(2) == support.size(2))
+    assert (n_support == n_way * n_shot)
+
     logit3 = None
     return logit3
-
-
-def FusionClsHead(query, support, support_labels, n_way, n_shot, normarlize=True):
-    logit_fusion = None
-    return logit_fusion
-
 
 
 class ClassificationHead(nn.Module):
@@ -109,14 +194,8 @@ class ClassificationHead(nn.Module):
         super(ClassificationHead, self).__init__()
         if 'proto' in base_learner:
             self.head = ProtoNetHead
-        if 'cls_head1' in base_learner:
-            self.head = classifition_head1
-        if 'cls_head2' in base_learner:
-            self.head = classifition_head2
-        if 'cls_head3' in base_learner:
-            self.head = classifition_head3
-        if 'fusion_head' in base_learner:
-            self.head = FusionClsHead
+        if 'cosin' in base_learner:
+            self.head = CosineSimilarity
         else:
             assert False
 
@@ -130,3 +209,12 @@ class ClassificationHead(nn.Module):
             return self.head(query, support, support_labels, n_way, n_shot, **kwargs)
 
 
+# if __name__ == '__main__':
+#     support = torch.randn(3, 3, 4).cuda()
+#     query = torch.randn(3, 3, 4).cuda()
+#     n_way = 3
+#     n_shot = 1
+#     normarlize = True
+#     support_labels = torch.LongTensor([[0, 1, 2], [1, 0, 2], [0, 2, 1]]).cuda()
+#     logit = CosineSimilarity(query, support, support_labels, n_way, n_shot, normarlize).cuda()
+#     print(logit.cpu())
